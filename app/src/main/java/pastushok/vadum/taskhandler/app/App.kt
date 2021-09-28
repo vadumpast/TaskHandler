210382f5197265bd4d85ac904a7faf007c9e774d
Vadum <vadumpast@gmail.com>
2021-09-28 10:24:20 +0300

    created project

---
+++ app/src/main/java/pastushok/vadum/taskhandler/app/App.kt
@@ -0,0 +1,9 @@
+package pastushok.vadum.taskhandler.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+
+}
---
+++ app/src/main/java/pastushok/vadum/taskhandler/base/BaseFragment.kt
@@ -0,0 +1,10 @@
+package pastushok.vadum.taskhandler.base
+
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding>: Fragment() {
+
+
+
+}
